91cedc5a2c6cf274ff7ab1acfd9881ef72195858
Irving Paim <irvingpaim@yahoo.com.br>
2023-05-28 20:50:51 -0300

    Criado script app.js para isolar o código

---
+++ watch/app.js
@@ -0,0 +1,23 @@
+function setClock () {
+    // Get the current time
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    //Calculate clock hands rotate values (according to 360deg)
+    //Using the current time
+
+    hrValue = (hours * 30) + (minutes * 6) / 12;
+    minValue = minutes * 6;
+    secValue = seconds * 6;
+
+    //Add calculeted rotate values to the clock hand elements
+
+    document.querySelector(".hr-hand").style.transform = "rotate(" + hrValue + "deg)";
+    document.querySelector(".min-hand").style.transform = "rotate(" + minValue + "deg)";
+    document.querySelector(".sec-hand").style.transform = "rotate(" + secValue + "deg)";
+    
+}
+
+setInterval(setClock, 1000);
